3783676f944885b777d3bfa857d70b8daf7ded43
Roelf O <r.otten@youwe.nl>
2015-10-02 16:46:30 +0200

    WIP dropping views and combining functionalities

--- lib/Deployment/Definition.php
+++ lib/Deployment/Definition.php
@@ -63,14 +63,73 @@ class Definition {
      */
     public function dropViews($classes = false) {
 
-        $views = $this->db->fetchAll("SELECT CONCAT(TABLE_SCHEMA,'.',TABLE_NAME) AS view
+        // TODO: grab mappings from json files
+
+//        $views = $this->db->fetchAll("SELECT CONCAT(TABLE_SCHEMA,'.',TABLE_NAME) AS view
+//                    FROM information_schema.TABLES
+//                    WHERE TABLE_TYPE = 'VIEW' AND TABLE_SCHEMA = " . $this->db->quote($this->db->getConfig()['dbname']));
+
+        $views = $this->db->fetchAll("SELECT
+                      CONCAT(TABLE_SCHEMA,'.',TABLE_NAME) AS fulltablename,
+                      TABLE_TYPE as ttype,
+                      TABLE_NAME as tablename
                     FROM information_schema.TABLES
-                    WHERE TABLE_TYPE = 'VIEW' AND TABLE_SCHEMA = " . $this->db->quote($this->db->getConfig()['dbname']));
+                    WHERE TABLE_SCHEMA = " . $this->db->quote($this->db->getConfig()['dbname']));
+
+//        $views = $this->db->fetchAll("SELECT
+//                      CONCAT(TABLE_SCHEMA,'.',TABLE_NAME) AS fulltablename,
+//                      TABLE_TYPE as ttype,
+//                      TABLE_NAME as tablename
+//                    FROM information_schema.TABLES
+//                    WHERE
+//                      (
+//                       TABLE_NAME REGEXP " . $this->db->quote("^object_[0-9]+$") . "
+//                        OR
+//                       TABLE_NAME REGEXP " . $this->db->quote("^object_localized_[0-9]+_[A-z]{2}_[A-z]{2}$") . "
+//                        OR
+//                       TABLE_NAME REGEXP " . $this->db->quote("^object_localized_[0-9]+_[A-z]{2}$") . "
+//                      )
+//                      AND
+//                      TABLE_SCHEMA = " . $this->db->quote($this->db->getConfig()['dbname']));
+
+//        ---- object_##
+//        ---- object_localized_##_XX_XX
+//        ---- object_localized_##_XX
 
         foreach ($views as $view) {
             // check if class needs to be skipped ($classes)
-            echo "Dropping view: " . $view['view'] . "\n";
-            $this->db->query('DROP VIEW IF EXISTS ' . $this->db->quoteIdentifier($view['view']))->execute();
+
+            $should_be_view = false;
+            if (preg_match('/^object_([0-9]+)$/', $view['tablename'], $matches)
+                    ||
+                preg_match('/^object_localized_([0-9]+)_[A-z]{2}$/', $view['tablename'], $matches)
+                    ||
+                preg_match('/^object_localized_([0-9]+)_[A-z]{2}_[A-z]{2}$/', $view['tablename'], $matches)
+            ) {
+                $should_be_view = true;
+                $class_id = $matches[1];
+            }
+
+            if($should_be_view)
+            {
+                echo "Found: [" . $view['ttype'] . "] " . $view['tablename'] . ": ";
+
+                // TODO grab class name from ID and check :
+                // if($classes && !in_array($class, $classes)) continue;
+
+                if($view['ttype'] == 'VIEW') {
+                    $this->db->query('DROP VIEW IF EXISTS ' . $this->db->quoteIdentifier($view['fulltablename']))->execute();
+                }
+                elseif($view['ttype'] == 'BASE TABLE') {
+                    $this->db->query('DROP TABLE IF EXISTS ' . $this->db->quoteIdentifier($view['fulltablename']))->execute();
+                }
+            }
+            else
+            {
+                //echo "skipping";
+            }
+            echo "\n";
+
         }
     }
 

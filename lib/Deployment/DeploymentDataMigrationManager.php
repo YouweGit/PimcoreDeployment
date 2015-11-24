5d92ee21ad7b6212f83ba9bbea3868314bb28a5e
Roelf O <r.otten@youwe.nl>
2015-11-24 16:48:52 +0100

    bug fixes

--- lib/Deployment/DeploymentDataMigrationManager.php
+++ lib/Deployment/DeploymentDataMigrationManager.php
@@ -30,32 +30,36 @@ class DeploymentDataMigrationManager {
     public static function setModeByCnameAndId($cname, $cid, $cid2 = null, $cid3 = null, $mode)
     {
         $deployment_data_object = self::retrieveObjectByCnameAndId($cname, $cid, $cid2, $cid3);
+//        var_dump($deployment_data_object);
+//        die();
+
         if(!$deployment_data_object) {
 
             // Create a new object (all related object migration keys will be created by the CLI)
+            $parent_id_of_new_object = \PathManager_PathManager::getOrCreateSubfolder('/deployment', 'datamigration');
 
             $deployment_data_object = new DeploymentDataMigration();
             $deployment_data_object->setCName($cname);
             $deployment_data_object->setMode($mode);
             $deployment_data_object->setCId($cid);
+            $deployment_data_object->setTimestamp(new \Pimcore\Date());
             $deployment_data_object->setMigrationKey(self::generateUniqueMigrationKey());
-            $deployment_data_object->setParentId();
-//            $deployment_data_object->setPath('/Deployment/DataMigration');
+            $deployment_data_object->setParentId($parent_id_of_new_object);
+
             if($cid2) $deployment_data_object->setCId2($cid2);
             if($cid3) $deployment_data_object->setCId2($cid3);
             $deployment_data_object->setKey($cname . '-' . $cid . '-' . $cid2 . '-' . $cid3);
             $deployment_data_object->save();
-//            die();
         }
     }
 
     public static function retrieveObjectByCnameAndId($cname, $cid, $cid2 = null, $cid3 = null)
     {
         $list = new DeploymentDataMigration\Listing();
-        $list->addConditionParam('CName', $cname);
-        $list->addConditionParam('CId', $cid);
-        if ($cid2) $list->addConditionParam('CId2', $cid2);
-        if ($cid3) $list->addConditionParam('CId3', $cid3);
+        $list->addConditionParam('CName = ?', $cname);
+        $list->addConditionParam('CId = ?', $cid);
+        if ($cid2) $list->addConditionParam('CId2 = ?', $cid2);
+        if ($cid3) $list->addConditionParam('CId3 = ?', $cid3);
         $deployment_data_object = $list->current();
         return $deployment_data_object;
     }
@@ -73,7 +77,7 @@ class DeploymentDataMigrationManager {
     {
         if(!$uid) return false;
         $deployment_data_object = DeploymentDataMigration::getByMigrationKey($uid);
-        if(!$deployment_data_object) return true;
+        if(!$deployment_data_object->count()) return true;
         return false;
     }
 

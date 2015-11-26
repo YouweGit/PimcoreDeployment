c8e01dc4d2851f4f52d6b0cfe1852fb496194863
Roelf O <r.otten@youwe.nl>
2015-11-26 08:54:09 +0100

    starting dev on dump

--- lib/Deployment/Content.php
+++ lib/Deployment/Content.php
@@ -2,6 +2,8 @@
 
 namespace Deployment;
 
+use Pimcore\Model\Object\DeploymentDataMigration;
+
 class Content extends \Deployment\DAbstract
 {
     /**
@@ -19,11 +21,13 @@ class Content extends \Deployment\DAbstract
     public $config;
 
     private $db;
+    private $ddmm;
 
     function __construct()
     {
         parent::__construct();
         $this->db = \Pimcore\Resource::get();
+        $this->ddmm = new DeploymentDataMigrationManager();
         $this->backupPath = PIMCORE_WEBSITE_PATH . $this->backupPath;
         $this->backupTmpPath = PIMCORE_WEBSITE_PATH . $this->backupTmpPath;
         \Pimcore\File::mkdir($this->backupPath);
@@ -35,7 +39,11 @@ class Content extends \Deployment\DAbstract
      */
     public function exportContent()
     {
+        $this->addMigrationKeys();
+
         $this->clearTmpDir();
+
+        $this->dumpContent();
 
         // dump some data to jsons
         file_put_contents($this->backupTmpPath . 'test.json', 'test-data');
@@ -81,6 +89,120 @@ class Content extends \Deployment\DAbstract
         $this->clearTmpDir();
     }
 
+    public function addMigrationKeys()
+    {
+        echo "\nCreating migration keys for all known content tables.\n";
+
+        $cts = array(
+            'documents'         => array(
+                'idfield'       => 'id',
+                'idfield2'      => null,
+                'idfield3'      => null
+            ),
+            'documents_link'    => array(
+                'idfield'       => 'id',
+                'idfield2'      => null,
+                'idfield3'      => null
+            ),
+            'documents_page'    => array(
+                'idfield'       => 'id',
+                'idfield2'      => null,
+                'idfield3'      => null
+            ),
+            'documents_elements' => array(
+                'idfield'       => 'documentId',
+                'idfield2'      => 'name',
+                'idfield3'      => null
+            ),
+            'properties'        => array(
+                'idfield'       => 'cid',
+                'idfield2'      => 'ctype',
+                'idfield3'      => 'name'
+            ),
+        );
+
+        foreach($cts as $tn => $ct) {
+
+            echo "table: $tn\n";
+
+            $sql = "SELECT * FROM " . $tn;
+            $docs = $this->db->fetchAssoc($sql);
+            foreach ($docs as &$doc) {
+                $id1 = $doc[$ct['idfield']];
+                $id2 = $ct['idfield2'] ? $doc[$ct['idfield2']] : null;
+                $id3 = $ct['idfield3'] ? $doc[$ct['idfield3']] : null;
+
+                echo $tn . ' ' .
+                    ' [' . $ct['idfield'] . '=' . $id1 . '] ' .
+                    ' [' . $ct['idfield2'] . '=' . $id2 . '] ' .
+                    ' [' . $ct['idfield3'] . '=' . $id3 . '] ';
+
+//                echo " ($id1-$id2-$id3) ";
+
+                // look up current migration setting by doc id
+                $migration_object = \Deployment\DeploymentDataMigrationManager::createKeyByCnameAndId($tn, $id1, $id2, $id3);
+
+                echo $migration_object->getMigrationKey() . "\n";
+
+            }
+
+        }
+
+    }
+
+    public function dumpContent()
+    {
+        echo "\nDumping content structure to json:\n";
+
+        $migslist = new DeploymentDataMigration\Listing();
+        $migslist->setUnpublished(true);
+        $migs = $migslist->getObjects();
+        foreach($migs as $mig)
+        {
+            if($mig->getMode() != 'default') {
+                $this->dumpContentPart($mig);
+            }
+        }
+    }
+
+
+    public function dumpContentPart($mig)
+    {
+        echo $mig->getCName() . " " . $mig->getCId() . " " . $mig->getCId2() . " " . $mig->getCId3() . " " . $mig->getMode() . "\n";
+        if($mig->getCName() == 'documents')
+        {
+            $this->dumpContentPartDocuments($mig);
+        }
+    }
+
+    public function dumpContentPartDocuments($mig)
+    {
+        $docdata = $this->ddmm->getDataByDDM($mig);
+        echo 'test ';
+        var_dump($docdata);
+        die();
+
+        // dump the data itself
+
+        // RELATED:
+        // parent documents (parentId)
+        if($docdata['parentId'] > 0)
+        {
+            $mig2 = DeploymentDataMigrationManager::retrieveObjectByCnameAndId('documents', $docdata['parentId']);
+            $this->dumpContentPart($mig2);
+        }
+
+        // documents_page (type == page? --> use id)
+
+        // documents_elements (documentid)
+
+        // -----------------------------------------------
+        // @TODO: complete the list of relations to export
+
+
+    }
+
+
     /**
      * Executes query
      * @param string $query
@@ -99,6 +221,8 @@ class Content extends \Deployment\DAbstract
     {
         array_map('unlink', glob($this->backupTmpPath . '*'));
     }
+
+
 
 
 }

38f5c0bd08199a6715fe7b9d04c462b32803fa17
Manea Eugen <e.manea@youwe.nl>
2017-02-03 14:38:47 +0100

    Run all imports permission fix

--- cli/run-all-imports.sh
+++ cli/run-all-imports.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 CURRENT_PATH=$(cd $(dirname "$0"); pwd)
-$CURRENT_PATH/clear-classes.sh
-$CURRENT_PATH/import-field-collection.sh
-$CURRENT_PATH/import-definition.sh
-$CURRENT_PATH/import-customlayout.sh
-$CURRENT_PATH/import-bricks.sh
-$CURRENT_PATH/import-staticdata.sh
-$CURRENT_PATH/import-customsql.sh
+/bin/sh $CURRENT_PATH/clear-classes.sh
+/bin/sh $CURRENT_PATH/import-field-collection.sh
+/bin/sh $CURRENT_PATH/import-definition.sh
+/bin/sh $CURRENT_PATH/import-customlayout.sh
+/bin/sh $CURRENT_PATH/import-bricks.sh
+/bin/sh $CURRENT_PATH/import-staticdata.sh
+/bin/sh $CURRENT_PATH/import-customsql.sh
